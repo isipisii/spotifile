c7dc36cdf066191085f66dab31632c49f7d4480b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-14 15:49:29 +0800

    added redux store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { spotifyApi } from "./services/spotify";
+import { tokenReducer } from "./token/tokenSlice";
 
 export const store = configureStore({
   reducer: {
     [spotifyApi.reducerPath]: spotifyApi.reducer,
+    token: tokenReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(spotifyApi.middleware),
