96df05ad9bb5955b2327d492f3fd170a1d2fe8ef
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-14 21:45:46 +0800

    added new data coming from different endpoint

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,47 @@
 "use client";
-import { useGetUserQuery } from "@/services/spotify";
-import { setAccessToken } from "@/token/tokenSlice";
+import { useGetUserQuery, useGetFollowingQuery, useGetPlaylistsQuery } from "@/services/spotify";
 import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 const Profile = () => {
-  const {data: session} = useSession();
-  const { data, error, isLoading } = useGetUserQuery(session?.accessToken);
-  
-  console.log(data);
+  const { data: session } = useSession();
+  const { data: userData } = useGetUserQuery(session?.accessToken);
+  const { data: followingData } = useGetFollowingQuery(session?.accessToken);
+  const { data: playlistsData } = useGetPlaylistsQuery(session?.accessToken);
+  console.log(userData)
+  console.log(playlistsData)
+  return (  
+    <section className="flex justify-end">
+      <div className="border-white border w-full md:w-[92%] md:ml-[100px] p-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-white font-bold text-[2rem]">Profile</h1>
+          <button className="font-medium text-white text-sm rounded-full py-2 px-5 transition-all duration-300 hover:text-black hover:bg-white border border-[#dad4d4]" onClick={signOut}>Log out</button>
+        </div>
 
-  return (
-    <div>
+        <div className="w-full flex items-center justify-center">
+          <div className="flex flex-col items-center">
+            <a href={userData?.external_urls?.spotify}>
+              <img
+                className="rounded-full h-[150px] w-[150px]"
+                src={userData?.images[0]?.url}
+                alt={`${userData?.display_name}'s profile`}
+              />
+            </a>
+            <h1 className="text-center text-white text-[4rem] font-bold">
+              {userData?.display_name}
+            </h1>
+            <div>
+              <p className="text-center text-sm font-medium text-white">
+                {userData?.followers?.total} Follower {" "}  • {" "} {followingData?.artists?.total} Following • {" "} {playlistsData?.total} Playlists
+              </p>
+            </div>
+          </div>
+        </div>
 
-    </div>
-  )
-}
 
-export default Profile
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
