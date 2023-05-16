c991b894812ed198f96136fae396af09108c770d
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-16 22:54:30 +0800

    Update page.jsx

--- src/app/playlists/page.jsx
+++ src/app/playlists/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
 const Playlists = () => {
-  return (
-    <div>Playlists</div>
-  )
-}
+  return <div>Playlists</div>;
+};
 
-export default Playlists
+export default Playlists;
+
+// export async function getStaticSideProps(context) {
+//   const session = await getSession(context)
+
+//   return {
+//     props: {
+//       session,
+//     },
+//   }
+// }
