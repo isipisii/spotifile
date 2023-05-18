b364047a012be347d49702e1ea744e2a10889651
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-18 12:02:12 +0800

    removed top artists

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,30 +11,30 @@ import { signOut } from "next-auth/react";
 import Link from "next/link";
 import moment from "moment";
 import { usePalette } from "@lauriys/react-palette";
-import { useState, useEffect } from "react";
+import TopArtists from "./TopArtists";
 
 const Profile = () => {
   const { data: session } = useSession();
   const { data: userData } = useGetUserQuery(session?.accessToken);
   const { data: followingData } = useGetFollowingQuery(session?.accessToken);
   const { data: playlistsData } = useGetPlaylistsQuery(session?.accessToken);
-  const { data: topArtistOfAllTime } = useGetTopArtistsOfAllTimeQuery({
-    accessToken: session?.accessToken,
-    length: 10,
-  });
   const { data: topTracks } = useGetUserTopTracksQuery({
     accessToken: session?.accessToken,
     length: 10,
   });
-  const { data: color, loading, error } = usePalette(userData?.images[0]?.url);
-  
+
+  const { data: color } = usePalette(userData?.images[0]?.url);
+
   return (
     <section className="flex items-center justify-center">
       <div className="relative w-full max-w-[1200px] md:w-[92%] md:ml-[100px] flex flex-col gap-10">
         {/* top part */}
         <div
           className="gradient-background"
-          style={{ '--from-color': color.darkVibrant, '--via-color': '#121212d1' }}
+          style={{
+            "--from-color": color.darkVibrant,
+            "--via-color": "#121212d1",
+          }}
         />
         <div className="flex items-center justify-between p-6 ">
           <h1 className="text-white font-bold text-[2rem]">Profile</h1>
@@ -73,48 +73,12 @@ const Profile = () => {
         {/* top artists and tracks */}
         <div className="my-6 flex flex-col p-6">
           {/* top artists */}
-          <div className="mb-[4rem]">
-            <div className="flex justify-between items-center mb-4">
-              <h1
-                className="text-white font-semibold
-                text-[1.1rem] md:text-[1.3rem]"
-              >
-                Top Artists of all time
-              </h1>
-              <Link
-                href="/top-artists"
-                className="text-[#cdc8c8] text-xs md:text-sm font-semibold"
-              >
-                See more
-              </Link>
-            </div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-4">
-              {topArtistOfAllTime?.items?.map((artist, index) => (
-                <a
-                  href={artist?.external_urls?.spotify}
-                  target="_blank"
-                  key={index}
-                >
-                  <div className="flex rounded-[7px] flex-col gap-1 items-center max-w-[250px] bg-[#0000002d] p-5 hover:bg-[#3534346f]">
-                    <img
-                      src={artist?.images[0]?.url}
-                      alt="artist-profile"
-                      className="rounded-[100%] object-cover h-[110px] w-[110px] sm:w-[120px] sm:h-[120px] md:h-[150px] md:w-[150px] lg:h-[150px] lg:w-[150px]"
-                    />
-                    <h3 className="text-white text-sm md:text-base mt-2 font-semibold">
-                      {artist?.name}
-                    </h3>
-                    <p className="text-[#cdc8c8] text-xs md:text-sm">
-                      {artist?.type.charAt(0).toUpperCase() +
-                        artist?.type.slice(1)}
-                    </p>
-                  </div>
-                </a>
-              ))}
-            </div>
-          </div>
+          <TopArtists
+            accessToken={session?.accessToken}
+            length={10}
+            render={true}
+          />
           {/* end of top artists */}
-
           {/* top tracks */}
           <div>
             <div className="flex justify-between items-center mb-4">
