67ea331be07da094141ed8509b3d7a2759df1c11
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-18 12:01:25 +0800

    styles

--- src/app/top-artists/page.jsx
+++ src/app/top-artists/page.jsx
@@ -1,11 +1,17 @@
+"use client";
+import TopArtists from "@/components/TopArtists";
+import { useSession } from "next-auth/react";
 
-const TopArtists = () => {
+const TopArtistsPage = () => {
+  const { data: session } = useSession();
+
   return (
-    <>
-      <div className="text-white">TopArtists</div>
-      <div className="text-white"></div>
-    </>
+    <section className="flex items-center justify-center">
+      <div className="relative w-full max-w-[1200px] md:w-[92%] md:ml-[100px] flex flex-col gap-10 ">
+        <TopArtists accessToken={session?.accessToken} length={20}/>
+      </div>
+    </section>
   );
 };
 
-export default TopArtists;
+export default TopArtistsPage;
