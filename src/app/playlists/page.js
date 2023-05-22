693c23cce4fc5c57d2264a58947d1a67d2c2aad6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-22 20:53:00 +0800

    Create page.js

---
+++ src/app/playlists/page.js
@@ -0,0 +1,16 @@
+
+const Playlists = () => {
+  return <div>Playlists</div>;
+};
+
+export default Playlists;
+
+// export async function getStaticSideProps(context) {
+//   const session = await getSession(context)
+
+//   return {
+//     props: {
+//       session,
+//     },
+//   }
+// }
