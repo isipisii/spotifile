e7e54c41dd08e619d4fafccb4c2ec59fc9fed388
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-23 14:21:44 +0800

    Update ArtistCard.jsx

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,7 +4,7 @@ const ArtistCard = ({ artist, index }) => {
   
   return (
     <a href={artist?.external_urls?.spotify} target="_blank">
-      <div className="flex rounded-[7px] flex-col gap-1 items-center max-w-[250px] bg-[#0000002d] p-5 hover:bg-[#3534346f] relative">
+      <div className="bg-[#0000002d] flex rounded-[7px] flex-col gap-1 items-center max-w-[250px] p-5 hover:bg-[#3534346f] relative">
         <p className="absolute top-2 left-2  text-sm text-[#b8b2b2]">#{index + 1}</p>
         <img
           src={artist?.images[0]?.url}
