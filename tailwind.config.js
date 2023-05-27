e4e4f7830511c5478a82fc877f79e721002a53ee
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-05-27 11:49:43 +0800

    config of clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontSize: {
+          dynamic: "clamp(3rem, 1.5rem + 4.8vw, 4.5rem)",
+      }
     },
   },
   plugins: [],
