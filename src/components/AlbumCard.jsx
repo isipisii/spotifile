1126f8f71c0b0f1ff49f00d2a8ede48030a3d209
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-07 18:01:04 +0800

    linked to the album page

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 
 const AlbumCard = ({ album }) => {
   const albumType = album?.album_type
@@ -8,15 +9,17 @@ const AlbumCard = ({ album }) => {
   const year = album?.release_date.split("-")[0];
 
   return (
-    <div className="bg-[#0000002d] flex rounded-[7px] flex-col gap-3  max-w-[250px] p-5 hover:bg-[#3534346f]">
-      <img src={album?.images[0]?.url} alt="" className="rounded-[4px]" />
-      <h2 className="text-white truncate w-[100px] sm:w-[150px] font-semibold text-left text-xs sm:text-sm">
-        {album?.name}
-      </h2>
-      <h3 className="text-[#adaaaa] text-[.7rem] sm:text-xs font-medium">
-        {albumType} • {year}
-      </h3>
-    </div>
+    <Link href={`/album/${album?.id}`}>
+      <div className="bg-[#0000002d] flex rounded-[7px] flex-col gap-3  max-w-[250px] p-5 hover:bg-[#3534346f]">
+        <img src={album?.images[0]?.url} alt="" className="rounded-[4px]" />
+        <h2 className="text-white truncate w-[100px] sm:w-[150px] font-semibold text-left text-xs sm:text-sm">
+          {album?.name}
+        </h2>
+        <h3 className="text-[#adaaaa] text-[.7rem] sm:text-xs font-medium">
+          {albumType} • {year}
+        </h3>
+      </div>
+    </Link>
   );
 };
 
