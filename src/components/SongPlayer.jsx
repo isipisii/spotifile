0daf7113b4774c86b03c8db7de12e273bb409a89
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-08 10:34:10 +0800

    added offset to play the track that was clicked

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -1,13 +1,16 @@
 "use client";
 import SpotifyWebPlayer from "react-spotify-web-playback";
 
-const SongPlayer = ({ accessToken, trackUris }) => {
+const SongPlayer = ({ accessToken, trackUris, currentSong }) => {
+
   return (
     <div className="fixed bottom-0 left-0 w-full z-10">
       {/* TODO */}
       {trackUris() && (
         <SpotifyWebPlayer
           token={accessToken}
+          magnifySliderOnHover={true}
+          offset={!currentSong ? 0 : currentSong}
           uris={trackUris()}
           styles={{
             activeColor: "#fff",
