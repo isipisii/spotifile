e7532d99b7195d01e78f0ca7954be3e3cc82e715
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-10 23:27:56 +0800

    rendered the album details component

--- src/app/album/[id]/page.js
+++ src/app/album/[id]/page.js
@@ -2,18 +2,14 @@
 import { useDispatch } from "react-redux";
 import { setAccessToken } from "@/slice/authSlice";
 import { useSession } from "next-auth/react";
-import { useGetAlbumDetailsQuery } from "@/services/spotify";
-import { useParams } from "next/navigation";
+import AlbumDetails from "@/components/AlbumDetails";
 
 const AlbumPage = () => {
   const { data: session } = useSession();
-  const params = useParams();
   const dispatch = useDispatch();
   dispatch(setAccessToken(session?.accessToken));
-  const { data } = useGetAlbumDetailsQuery(params.id);
   
-  console.log(data);
-  return <div>page</div>;
+  return <AlbumDetails accessToken={session?.accessToken} />;
 };
 
 export default AlbumPage;
