bbbaae73adfb5f336e7f1c2053a98c4b69702258
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-11 21:16:33 +0800

    added skeleton loader

--- src/components/PlaylistDetails.jsx
+++ src/components/PlaylistDetails.jsx
@@ -3,41 +3,48 @@ import { useParams } from "next/navigation";
 import {
   useGetPlaylistDetailsQuery,
   useGetUserByIdQuery,
-  useLazyGetCurrentlyPlayingTrackQuery,
+  useGetCurrentlyPlayingTrackQuery,
 } from "@/services/spotify";
 import { usePalette } from "@lauriys/react-palette";
+import { useState, useEffect } from "react";
+
+import DetailLoader from "./Loaders/DetailLoader";
 import TrackCardLoader from "./Loaders/TrackCardLoader";
 import Track from "./Track";
 import SongPlayer from "./SongPlayer";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const PlaylistDetails = ({ session }) => {
   const params = useParams();
-  const { data: playlistDetails, isLoading: isPlaylistLoading } = useGetPlaylistDetailsQuery(params.id);
-  const { data: userDetails } = useGetUserByIdQuery(playlistDetails?.owner?.id);
-  const [getCurrentlyPlaying, { data: currentlyPlaying }] = useLazyGetCurrentlyPlayingTrackQuery();
+  const {
+    data: playlistDetails,
+    isLoading: isPlaylistLoading,
+    refetch: refetchPlaylistDetails,
+  } = useGetPlaylistDetailsQuery(params.id, session?.accessToken && session);
+  const {
+    data: userDetails,
+    isLoading: isUserLoading,
+    refetch: refetchUserDetails,
+  } = useGetUserByIdQuery(
+    playlistDetails?.owner?.id,
+    session?.accessToken && session
+  );
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+
+  const { data: currentlyPlaying } = useGetCurrentlyPlayingTrackQuery(null, {
+    pollingInterval: 1000,
+  });
 
   const playlistImage = playlistDetails?.images[0]?.url;
   const { data: color } = usePalette(playlistImage);
-  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
 
-  // get currently playing track in the first render
-  //BUGGGG
+  console.log(currentlyPlaying);
+
   useEffect(() => {
-    getCurrentlyPlaying();
-  }, [currentTrackIndex]);
-
-
-  //BUGGGG
-  function handleClickTrack(index) {
-    setCurrentTrackIndex(index);
-    
-    //in order to get the current track after the state updated to avoid fetching the last tracked played
-    setTimeout(() => {
-      getCurrentlyPlaying();
-    }, 500);
-  }
+    if (session?.accessToken) {
+      refetchPlaylistDetails();
+      refetchUserDetails();
+    }
+  }, [session?.accessToken]);
 
   //for collecting the uris of different tracks inside the playlist
   function getSongUris() {
@@ -65,37 +72,44 @@ const PlaylistDetails = ({ session }) => {
           trackUris={getSongUris}
           currentTrackIndex={currentTrackIndex}
         />
+
         <div className="flex flex-col md:items-end md:flex-row gap-4 md:gap-8 my-8">
-          <img
-            src={playlistDetails?.images[0]?.url}
-            className=" object-cover h-[200px] w-[200px] md:h-[250px] md:w-[250px] shadow-xl shadow-[#0202024d]"
-          />
-          <div className="flex flex-col gap-3">
-            <div>
-              <p className="text-[#ffffffba] font-light text-xs sm:text-sm md:text-base">
-                Playlist
-              </p>
-              <h1
-                className={`text-white ${
-                  playlistDetails?.name.length > 18
-                    ? "text-[1.8rem] sm:text-[3rem] lg:text-[3.5rem]"
-                    : "text-[2.5rem] sm:text-[3.5rem] lg:text-[4.5rem]"
-                } font-[800] text-shadow-md`}
-              >
-                {playlistDetails?.name}
-              </h1>
-            </div>
-            <div className="flex items-center gap-2">
+          {isPlaylistLoading || !playlistDetails ? (
+            <DetailLoader />
+          ) : (
+            <>
               <img
-                src={userDetails?.images[0]?.url}
-                alt=""
-                className="rounded-full h-[30px] w-[30px]"
+                src={playlistDetails?.images[0]?.url}
+                className=" object-cover h-[200px] w-[200px] md:h-[250px] md:w-[250px] shadow-xl shadow-[#0202024d]"
               />
-              <p className="text-white">
-                {playlistDetails?.owner?.display_name}
-              </p>
-            </div>
-          </div>
+              <div className="flex flex-col gap-3">
+                <div>
+                  <p className="text-[#ffffffba] font-light text-xs sm:text-sm md:text-base">
+                    Playlist
+                  </p>
+                  <h1
+                    className={`text-white ${
+                      playlistDetails?.name.length > 18
+                        ? "text-[1.8rem] sm:text-[3rem] lg:text-[3.5rem]"
+                        : "text-[2.5rem] sm:text-[3.5rem] lg:text-[4.5rem]"
+                    } font-[800] text-shadow-md`}
+                  >
+                    {playlistDetails?.name}
+                  </h1>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img
+                    src={userDetails?.images[0]?.url}
+                    alt=""
+                    className="rounded-full h-[30px] w-[30px]"
+                  />
+                  <p className="text-white">
+                    {playlistDetails?.owner?.display_name}
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
         </div>
         {/* end of upper part */}
         {/* tracks */}
@@ -121,7 +135,8 @@ const PlaylistDetails = ({ session }) => {
                   key={index}
                   index={index}
                   renderCount={true}
-                  handleClickTrack={handleClickTrack}
+                  setCurrentTrackIndex={setCurrentTrackIndex}
+                  currentlyPlaying={currentlyPlaying}
                 />
               ))}
             </div>
