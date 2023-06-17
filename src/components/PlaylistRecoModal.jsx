c6780a5722678c9ed8447a3d6c154209ea1445af
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-17 23:20:34 +0800

    created a modal for playlist reco

---
+++ src/components/PlaylistRecoModal.jsx
@@ -0,0 +1,91 @@
+"use client";
+import TrackCardLoader from "./Loaders/TrackCardLoader";
+import Track from "./Track";
+import { useNotify } from "@/hooks/useNotify";
+
+import {
+  useGetTrackRecommendationsQuery,
+  useCreatePlaylistMutation,
+  useAddTracksToPlaylistMutation,
+} from "@/services/spotify";
+
+const PlaylistRecoModal = ({
+  generateTrackIdsForRecommendation,
+  fromPlaylistName,
+  userId,
+}) => {
+  const { data: trackRecommendation, isLoading: isTrackRecoLoading } =
+    useGetTrackRecommendationsQuery({
+      length: 50,
+      trackIds: generateTrackIdsForRecommendation(),
+    });
+  const [createPlaylist] = useCreatePlaylistMutation();
+  const [addTracksToPlaylist] = useAddTracksToPlaylistMutation();
+  const [notificationMessage, makeNotification] = useNotify();
+
+  function getTrackRecommendedUris() {
+    return trackRecommendation?.tracks?.map((track) => track?.uri);
+  }
+
+  async function saveRecommendedTrack(id) {
+    try {
+      const createdPlaylistPayload = await createPlaylist(id).unwrap();
+
+      if (createdPlaylistPayload) {
+        await addTracksToPlaylist({
+          playlistId: createdPlaylistPayload?.id,
+          trackUris: getTrackRecommendedUris(),
+        });
+      }
+      console.log(isSuccess);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      makeNotification("Playlist saved successfully");
+    }
+  }
+
+  return (
+    <div className="max-w-[900px] w-[90%] fixed z-20  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#282828] p-6 rounded-xl flex flex-col gap-6">
+      {/* notification */}
+      {notificationMessage && (
+        <div className="z-20 fixed bottom-2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blur-sm bg-green-600 rounded-md p-3 md:p-4">
+          <p className="text-white text-[.6rem] sm:text-[.7rem] font-bold">
+            {notificationMessage}
+          </p>
+        </div>
+      )}
+
+      <h1 className="text-white font-bold text-[1.3rem]">
+        Recommended playlist based on {fromPlaylistName}
+      </h1>
+      <div className="flex flex-col gap-4">
+        <div className="max-h-[300px] overflow-y-auto mb-8 md:mb-0">
+          {/* tracks container */}
+          {isTrackRecoLoading || !trackRecommendation ? (
+            <div className="flex flex-col gap-1">
+              {[...new Array(5)].map((_, index) => (
+                <TrackCardLoader key={index} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col">
+              {/* track */}
+              {trackRecommendation?.tracks.map((track, index) => (
+                <Track track={track} key={index} showAlbum={true} />
+              ))}
+            </div>
+          )}
+        </div>
+        <button
+          className="text-black self-end font-semibold bg-white rounded-full py-3 px-4 text-[.9rem]"
+          onClick={() => saveRecommendedTrack(userId)}
+        >
+          Save to Spotify
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PlaylistRecoModal;
