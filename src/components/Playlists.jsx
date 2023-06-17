e77f0932d0d0d1d413b6400c501b2fcfa599bf68
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-17 23:20:47 +0800

    minor ui chnages

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -23,23 +23,24 @@ const Playlists = ({ session }) => {
       <div className="relative w-full max-w-[1400px] md:w-[92%] md:ml-[100px] flex flex-col gap-4 p-8">
         <div className="flex justify-between items-center ">
           <h1 className="text-white font-bold text-[1.6rem] text-center">
-            Playlist
+            Playlistss
           </h1>
         </div>
         {/* Playlist */}
         {/* playlist container */}
         {isPlayListsLoading || !playLists ? (
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-12">
-            {[...new Array(5)].map((_, index) => (
+            {[...new Array(10)].map((_, index) => (
               <PlaylistCardLoader key={index} />
             ))}
           </div>
         ) : (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mb-12">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4  gap-4 mb-12">
             {playLists?.items.map((playList, index) => (
+              // playlist card
               <Link href={`/playlist/${playList?.id}`} key={index}>
                 <div
-                  className="bg-[#0000002d] flex rounded-[7px] flex-col gap-3 max-w-[300px] p-4 hover:bg-[#3534346f]"
+                  className="bg-[#0000002d] flex rounded-[7px] flex-col gap-3 max-w-[300px] p-4 hover:bg-[#3534346f] transition-all duration-300"
                 >
                   <img
                     loading="lazy"
