8053f1caf3df338adaf62c3b67803fe989d7e3e9
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-17 23:18:51 +0800

    minor changes

--- src/components/Loaders/PlaylistCardLoader.jsx
+++ src/components/Loaders/PlaylistCardLoader.jsx
@@ -3,9 +3,9 @@
 const PlaylistCardLoader = () => {
   return (
     <div className="bg-[#0000002d] flex rounded-[7px] flex-col gap-4 min-w-[150px]  max-w-[250px] p-5">
-      <div className="animate-pulse bg-[#302e2ec0] w-[100%]  h-[120px] sm:h-[180px]  rounded-[4px]"></div>
-      <div className="animate-pulse bg-[#302e2ec0] w-[100px] h-4 rounded-sm"></div>
-      <div className="animate-pulse bg-[#302e2ec0] w-[90px] h-3 rounded-sm"></div>
+      <div className="animate-pulse bg-[#302e2ec0] w-[100%]  h-[120px] sm:h-[180px] rounded-[4px]"></div>
+      <div className="animate-pulse bg-[#302e2ec0] w-[100px] h-3 md:h-[4] rounded-sm"></div>
+      <div className="animate-pulse bg-[#302e2ec0] w-[90px] h-2 md:h-[3] rounded-sm"></div>
     </div>
   );
 };
