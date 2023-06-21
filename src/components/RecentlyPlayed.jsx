9c1781b3fbd88542bedb534492ce867aed9d473d
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-21 21:53:04 +0800

    added loader for recently played track

--- src/components/RecentlyPlayed.jsx
+++ src/components/RecentlyPlayed.jsx
@@ -1,13 +1,19 @@
-"use client"
+"use client";
 import { useGetRecentlyPlayedQuery } from "@/services/spotify";
 import { useEffect } from "react";
 import Track from "./Track";
 import { usePalette } from "@lauriys/react-palette";
+import TrackCardLoader from "./Loaders/TrackCardLoader";
 
 const RecentlyPlayed = ({ session }) => {
-  const { data: recentlyPlayed, isLoading, refetch } = useGetRecentlyPlayedQuery(session?.accessToken && session);
-  const mostRecentTrackImage = recentlyPlayed?.items[0]?.track?.album?.images[0]?.url
-  const {data: color} = usePalette(mostRecentTrackImage)
+  const {
+    data: recentlyPlayed,
+    isLoading,
+    refetch,
+  } = useGetRecentlyPlayedQuery(session?.accessToken && session);
+  const mostRecentTrackImage =
+    recentlyPlayed?.items[0]?.track?.album?.images[0]?.url;
+  const { data: color } = usePalette(mostRecentTrackImage);
 
   // refetch data when session changes and accessToken is available
   useEffect(() => {
@@ -33,12 +39,21 @@ const RecentlyPlayed = ({ session }) => {
         {/* Tracks */}
         <div className="max-h-[700px] md:max-h-[600px] h-full overflow-y-auto mb-12 md:mb-0">
           {/* tracks container */}
-          <div className="flex flex-col">
-            {/* track */}
-            {recentlyPlayed?.items.map((track, index) => (
-              <Track track={track?.track} key={index} renderCount={false}/>
-            ))}
-          </div>
+
+          {/* track */}
+          {isLoading || !recentlyPlayed ? (
+            <div className="flex flex-col gap-1">
+              {[...new Array(20)].map((_, index) => (
+                <TrackCardLoader key={index} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col">
+              {recentlyPlayed?.items.map((track, index) => (
+                <Track track={track?.track} key={index} renderCount={false} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </section>
