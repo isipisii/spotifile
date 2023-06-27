7e2972f3f2bc2b61a7e14fc9a5ebe4bf9963e027
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-27 19:19:14 +0800

    added modal reducer

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { spotifyApi } from "./services/spotify"
 import { authReducer } from "./slice/authSlice";
+import { modalReducers } from "./slice/modalSlice";
 
 export const store = configureStore({
   reducer: {
     [spotifyApi.reducerPath]: spotifyApi.reducer,
     auth: authReducer,
+    modal: modalReducers
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(spotifyApi.middleware),
